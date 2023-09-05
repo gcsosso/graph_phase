b8e666acdb8d5232a7594584f2082661a5379477
anwanguow <An.Wang@warwick.ac.uk>
2023-09-05 17:14:44 +0100

    Delete All files

--- LJ_system/graph/convert/do.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-filelist=`ls /storage/chem/msrgxt/dcd_files`
-for file in $filelist
-do
-	./convert "/storage/chem/msrgxt/dcd_files/$file" "/storage/chem/msrgxt/exyz_files/$file.xyz"
-	echo "$file is done."
-done
-
