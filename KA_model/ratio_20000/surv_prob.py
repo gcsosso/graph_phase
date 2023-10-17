d508b476448d785bc6bbd81aa8ab84a77e1c8d1b
anwanguow <143639727+anwanguow@users.noreply.github.com>
2023-10-17 16:01:32 +0100

    Resubmit the data

---
+++ KA_model/ratio_20000/surv_prob.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import linecache as lc
+
+gap = 0.01;
+T_min = 0;
+T_max = 1 + gap;
+sep = np.floor((T_max - T_min) / gap).astype(np.int16);
+T = np.zeros(sep, dtype = "float64");
+T[0] = T_min;
+T[1] = T_min + gap;
+for i in range(2, sep):
+    T[i] = T[i-1] + gap;
+T = T.astype(np.float32);
+
+thr = 1000;
+num_sim = 10;
+
+prob_sur = np.zeros((sep), dtype = "float32");
+
+for prob_b in range(sep):
+    text_ = 0;
+    count = 0;
+    for sim in range(num_sim):
+        file = "sim_" + str(int(sim)) + "/simulation/T_" + str(T[prob_b]) + "/nuc.log";
+        txt = str.split(lc.getline(file, 2224));
+        txt = txt[6];
+        if txt == "-1e+20":
+            text_ = 0;
+        else:
+            text_ = np.int32(txt);
+        if text_ > thr:
+            count = count + 1;
+    prob_sur[prob_b] = 1 - count / num_sim;
+
+plt.plot(T, prob_sur)
+np.save("swap/surv_prob.npy", prob_sur, allow_pickle=True);
+plt.savefig("/Users/dmr/Desktop/ans.png", dpi=300);
+plt.show()
+
---
+++ KA_model/ratio_50000/draw.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+surv_prob = np.load("swap/surv_prob.npy", allow_pickle=True)
+diff = np.load("swap/sop_diff.npy", allow_pickle=True)
+sop =np.load("swap/sop.npy", allow_pickle=True)
+
+gap = 0.01;
+T_min = 0;
+T_max = 1 + gap;
+sep = np.floor((T_max - T_min) / gap).astype(np.int16);
+T = np.zeros(sep, dtype = "float64");
+T[0] = T_min;
+T[1] = T_min + gap;
+for i in range(2, sep):
+    T[i] = T[i-1] + gap;
+T = T.astype(np.float32);
+
+
+
+diff = (diff - np.min(diff)) / (np.max(diff)-np.min(diff))
+sop = (sop - np.min(sop)) / (np.max(sop)-np.min(sop))
+
+plt.figure(figsize=(8,8))
+
+plt.plot(T, surv_prob)
+plt.plot(T, diff)
+
+plt.title("$\mathcal{T} = 50000$")
+plt.xlabel("Proportion of B particles p")
+plt.ylabel("Normalized measurements") 
+
+fig_1 ,= plt.plot(T, diff)
+fig_2 ,= plt.plot(T, sop)
+fig_3 ,= plt.plot(T, surv_prob)
+
+plt.legend(handles=[fig_1,fig_2,fig_3],labels=['$\partial \mathcal{D}_s / \partial p$', '$\mathcal{D}_s$', 'Survivial Prob'],loc='best')
+
+# plt.savefig("/Users/dmr/Desktop/ans.png", dpi = 300)
+plt.show()
+
